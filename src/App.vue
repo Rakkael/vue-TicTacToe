2f97a15e61da6d0157696accad1788821930b690
Rakkael <494844974@qq.com>
2020-08-02 22:17:55 +0800

    完成井字棋

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="game">
+      <div class="row">
+        <cell @countAdd="countSelfPlus(0, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(1, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(2, $event)" :data="this.count"></cell>
+      </div>
+      <div class="row">
+        <cell @countAdd="countSelfPlus(3, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(4, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(5, $event)" :data="this.count"></cell>
+      </div>
+      <div class="row">
+        <cell @countAdd="countSelfPlus(6, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(7, $event)" :data="this.count"></cell>
+        <cell @countAdd="countSelfPlus(8, $event)" :data="this.count"></cell>
+      </div>
+    </div>
+    <div>{{result}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from './components/Box.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Cell
+  },
+  data() {
+    return {
+      count: 0,
+      map: [
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+      ],
+      result: false
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    countSelfPlus(num, type){
+      this.count += 1;
+      this.map[Math.floor(num/3)][num%3] = type;
+      console.log(`第${num}个被点击，结果为${type}`);
+      this.whoWin();
+    },
+    whoWin(){
+      for(let i=0; i<3; ++i){
+        if(this.map[i][0] != null && this.map[i][0] == this.map[i][1] && this.map[i][1] == this.map[i][2]){
+          this.result = true;
+        }
+      };
+      for(let j=0; j<3; ++j){
+        if(this.map[0][j] != null && this.map[0][j] == this.map[1][j] && this.map[1][j] == this.map[2][j]){
+          this.result = true;
+        }
+      if(this.map[0][0] != null && this.map[0][0] == this.map[1][1] && this.map[1][1] == this.map[2][2]){
+        this.result = true;
+      }
+      if(this.map[0][2] != null && this.map[0][2] == this.map[1][1] && this.map[1][1] == this.map[2][0]){
+        this.result = true;
+      }
+      };
+    }
+  },
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.row {
+  display: flex;
 }
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="box" @click="changeState">{{ a }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      state: true,
+      a: ''
+    }
+  },
+  props:['data'],
+   methods: {
+      changeState(){
+        if(this.state !== true) return
+        if(this.data%2 === 0){
+          this.a = '×';
+          this.$emit('countAdd',this.a);
+        }else{
+          this.a = '○';
+          this.$emit('countAdd',this.a);
+        }
+        this.state = false;
+      }
+    },
+}
+</script>
+
+<style scoped>
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  font-size: 80px;
+  color: black;
+}
+</style>
